0dc75575a7b6d43c0d8398c1b326034c5682b6d2
gregfinley <greg@thefinleys.ca>
2014-02-21 09:44:13 -0500

    Add project file

--- insteon-utils/src/InsteonHubProxy.java
+++ insteon-utils/src/InsteonHubProxy.java
@@ -1,8 +1,30 @@
+import java.net.*;
+import java.io.*;
 
 public class InsteonHubProxy {
+	private static String downstreamHost = "";
+	private static int downstreamPort;
 
+	public void run() {
+		try {
+			// Create a ServerSocket to receive data
+			ServerSocket ss = new ServerSocket(9761);
+			
+			// Create buffers for client-to-server and server-to-client communication.
+			// We make one final so it can be use in an anonymous class below
+			
+			
+		} catch (Exception e) {
+			System.out.println("Uh Oh!");
+		}
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		if ((args[0].length() == 0) || (args[1].length() == 0)) {
+			System.out.println("Usage: <downstreamHost> <downstreamPort>");
+			System.exit(-1);
+		}
+		
 
 	}
 
